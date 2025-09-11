9a5ec3b3800a62361a53859dafac6f9561090768
nuuuwan <nuuuwan@gmail.com>
2025-09-11 11:32:54 +0530

    fix unittest data

--- tests/appeals/test_appeals_doc.py
+++ tests/appeals/test_appeals_doc.py
@@ -20,19 +20,11 @@ class TestCase(unittest.TestCase):
                 {
                     "num": "C.A. WRIT 388/2020",
                     "date_str": "2021-01-26",
-                    "description": "K.M.S. Bandara Vs Air Marshal D.L.S. Dias,  Commander of the Air Force and Others - before Hon. Sobhitha Rajakaruna, J",
-                    "url_pdf": "/?melsta_doc_download=1&doc_id=90b94e44-e1d2-478a-a1ac-23379f1beb6f&filename=c.a writ  388-2019.pdf.pdf",
-                    "parties": "K.M.S. Bandara Vs Air Marshal D.L.S. Dias,  Commander of the Air Force and Others -",
+                    "description": "K.M.S. Bandara Vs Air Marshal D.L.S. Dias,  Commander of the Air Force and Others - before Hon. Sobhitha Rajakaruna, J",  # noqa: E501
+                    "url_pdf": "https://courtofappeal.lk/?melsta_doc_download=1&doc_id=90b94e44-e1d2-478a-a1ac-23379f1beb6f&filename=c.a writ  388-2019.pdf.pdf",  # noqa: E501
+                    "parties": "K.M.S. Bandara Vs Air Marshal D.L.S. Dias,  Commander of the Air Force and Others -",  # noqa: E501
                     "judgement_by": "Hon. Sobhitha Rajakaruna, J",
                     "keywords": "",
                     "legistation": "",
                 },
             )
-
-        # hack
-        # https://courtofappeal.lk/?page_id=10834
-
-        for doc in doc_list:
-            if not doc.url_pdf.startswith("https://courtofappeal.lk"):
-                doc.url_pdf = "https://courtofappeal.lk" + doc.url_pdf
-                doc.write()
