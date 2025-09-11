11476469e56f6a568506dd98383f35e27092df41
nuuuwan <nuuuwan@gmail.com>
2025-09-11 09:56:12 +0530

    Added soup and html

--- src/utils_future/WebPage.py
+++ src/utils_future/WebPage.py
@@ -1,6 +1,24 @@
+from functools import cached_property
+
+import requests
+from bs4 import BeautifulSoup
+
+
 class WebPage:
+    TIMEOUT = 120
+
     def __init__(self, url):
         self.url = url
 
     def __str__(self):
         return f"🌐 {self.url}"
+
+    @cached_property
+    def html(self):
+        response = requests.get(self.url, timeout=self.TIMEOUT)
+        response.raise_for_status()
+        return response.text
+
+    @cached_property
+    def soup(self):
+        return BeautifulSoup(self.html, "html.parser")
