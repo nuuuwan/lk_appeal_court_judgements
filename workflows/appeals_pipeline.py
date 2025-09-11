70c45ae31ef3f1f31e6881fbb91e1e21508046c2
nuuuwan <nuuuwan@gmail.com>
2025-09-11 10:40:55 +0530

    Added max_dt

--- workflows/appeals_pipeline.py
+++ workflows/appeals_pipeline.py
@@ -1,4 +1,6 @@
+import sys
+
 from appeals import AppealsPipeline
 
 if __name__ == "__main__":
-    AppealsPipeline().run()
+    AppealsPipeline().run(max_dt=sys.argv[1] if len(sys.argv) > 1 else None)
