8005d1c53418fd3586b1b42d4eec11a23aaa716e
nuuuwan <nuuuwan@gmail.com>
2025-09-11 10:42:37 +0530

    more data

--- workflows/appeals_pipeline.py
+++ workflows/appeals_pipeline.py
@@ -3,4 +3,6 @@ import sys
 from appeals import AppealsPipeline
 
 if __name__ == "__main__":
-    AppealsPipeline().run(max_dt=sys.argv[1] if len(sys.argv) > 1 else None)
+    AppealsPipeline().run(
+        max_dt=int(sys.argv[1]) if len(sys.argv) > 1 else None
+    )
