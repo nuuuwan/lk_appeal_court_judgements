887daf5857f44abe09029dd0e27eefa97160985e
nuuuwan <nuuuwan@gmail.com>
2025-09-11 12:13:00 +0530

    ReadMe logic

--- workflows/appeals_pipeline.py
+++ workflows/appeals_pipeline.py
@@ -1,8 +1,10 @@
 import sys
 
-from appeals import AppealsPipeline
+from appeals import AppealsDoc, AppealsHomePage, AppealsPipeline
+from pdf_scraper import ReadMe
 
 if __name__ == "__main__":
     AppealsPipeline().run(
         max_dt=int(sys.argv[1]) if len(sys.argv) > 1 else None
     )
+    ReadMe(AppealsHomePage, AppealsDoc).build()
--- workflows/appeals_readme_build.py
+++ workflows/appeals_readme_build.py
@@ -2,5 +2,4 @@ from appeals import AppealsDoc, AppealsHomePage
 from pdf_scraper import ReadMe
 
 if __name__ == "__main__":
-    readme = ReadMe(AppealsHomePage, AppealsDoc)
-    readme.build()
+    ReadMe(AppealsHomePage, AppealsDoc).build()
