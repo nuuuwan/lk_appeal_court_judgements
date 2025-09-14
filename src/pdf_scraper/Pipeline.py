1468039868cf2ae2dc38cbbdc4a863ad1eb92acb
nuuuwan <nuuuwan@gmail.com>
2025-09-14 14:55:26 +0530

    Added HuggingFaceDataset to Pipeline

--- src/pdf_scraper/Pipeline.py
+++ src/pdf_scraper/Pipeline.py
@@ -4,6 +4,7 @@ from utils import Log
 
 from pdf_scraper.AbstractDoc import AbstractDoc
 from pdf_scraper.AbstractHomePage import AbstractHomePage
+from pdf_scraper.HuggingFaceDataset import HuggingFaceDataset
 from pdf_scraper.PipelineExtendedDataMixin import PipelineExtendedDataMixin
 from pdf_scraper.PipelineMetadataMixin import PipelineMetadataMixin
 from pdf_scraper.ReadMe import ReadMe
@@ -30,3 +31,4 @@ class Pipeline(PipelineMetadataMixin, PipelineExtendedDataMixin):
         self.__scrape_metadata__(max_dt)
         self.__scrape_extended_data__(max_dt)
         ReadMe(self.home_page_class, self.doc_class).build()
+        HuggingFaceDataset(self.doc_class).build_and_upload()
