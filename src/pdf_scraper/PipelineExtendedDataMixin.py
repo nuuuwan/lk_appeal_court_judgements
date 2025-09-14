a797589eeb51f571064aaa04936ea8350e12c20b
nuuuwan <nuuuwan@gmail.com>
2025-09-14 09:52:06 +0530

    Removed unused

--- src/pdf_scraper/PipelineExtendedDataMixin.py
+++ src/pdf_scraper/PipelineExtendedDataMixin.py
@@ -9,8 +9,7 @@ class PipelineExtendedDataMixin:
     def __scrape_extended_data__(self, max_dt):
         t_start = time.time()
         doc_list = self.doc_class.list_all()
-        n = len(doc_list)
-        for i_doc, doc in enumerate(doc_list, start=1):
+        for doc in doc_list:
             doc.scrape_extended_data()
             dt = time.time() - t_start
             if dt > max_dt:
