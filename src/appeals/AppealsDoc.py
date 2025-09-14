b8236377413ea086ecf1ef2a49a7fa60099df2ef
nuuuwan <nuuuwan@gmail.com>
2025-09-14 09:18:17 +0530

    Added remote_data_url_base

--- src/appeals/AppealsDoc.py
+++ src/appeals/AppealsDoc.py
@@ -16,12 +16,24 @@ class AppealsDoc(AbstractDoc):
     def doc_class_pretty_label(cls) -> str:
         return "⚖️ Judgements of the Court of Appeal of 🇱🇰 Sri Lanka"
 
+    @classmethod
+    @cache
+    def remote_data_url_base(cls) -> str:
+        return "/".join(
+            [
+                "https://github.com",
+                "nuuuwan",
+                "lk_judiciary_appeals_court",
+                "tree",
+                "data",  # branch-name
+            ]
+        )
+
     @cached_property
     def remote_data_url(self) -> str:
         return "/".join(
             [
-                "https://github.com"
-                + "/nuuuwan/lk_judiciary_appeals_court/tree/data/",
+                self.remote_data_url_base(),
                 self.dir_doc_extended_without_base,
             ]
         )
