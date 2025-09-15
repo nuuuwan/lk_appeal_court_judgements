0cb0c6ba3853f977e75386d42fd09e1360ec766e
nuuuwan <nuuuwan@gmail.com>
2025-09-15 16:43:04 +0530

    updates from pdf_scraper

--- README.md
+++ README.md
@@ -7,7 +7,7 @@
 | Data Source | https://courtofappeal.lk |
 | Date Range | 2012-04-23 to 2025-09-11 |
 | Number of Docs | 10,099 |
-| Dataset Size | 3.8GB |
+| Dataset Size | 1.4GB |
 
 ## Document Metadata Example
 
--- src/pdf_scraper/abstract_doc/AbstractDocMetadataMixin.py
+++ src/pdf_scraper/abstract_doc/AbstractDocMetadataMixin.py
@@ -73,7 +73,6 @@ class AbstractDocMetadataMixin:
         doc_list = [
             cls.from_file(json_path) for json_path in cls.get_all_json_paths()
         ]
-        doc_list = [doc for doc in doc_list if doc is not None]
         doc_list.sort(key=lambda doc: (doc.doc_id), reverse=True)
         return doc_list
 
