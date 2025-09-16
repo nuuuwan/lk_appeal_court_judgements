b0440a5ab07de3c4c155439bd71ca7e8973710d4
nuuuwan <nuuuwan@gmail.com>
2025-09-16 15:24:12 +0530

    data_repo_push

--- workflows/appeals_pipeline_local.py
+++ workflows/appeals_pipeline_local.py
@@ -38,7 +38,23 @@ def extended_data_repo_push():
     )
 
 
+def data_repo_push():
+    log.debug("data_repo_push...")
+    os.system(
+        " && ".join(
+            [
+                "git add .",
+                "git commit -m '👶🏾 [appeals_pipeline_local]'",
+                "git pull origin data --rebase",
+                "git push origin data",
+                "cd -",
+            ]
+        )
+    )
+
+
 if __name__ == "__main__":
     extended_data_repo_pull()
     AppealsDoc.run_pipeline()
     extended_data_repo_push()
+    data_repo_push()
