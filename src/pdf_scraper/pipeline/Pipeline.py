fb4a9dccd798a1fff6d42a4c5a2b7c0484b55b2f
nuuuwan <nuuuwan@gmail.com>
2025-09-16 09:39:23 +0530

    Added cleanup, lint

--- src/pdf_scraper/pipeline/Pipeline.py
+++ src/pdf_scraper/pipeline/Pipeline.py
@@ -5,16 +5,18 @@ from utils import Log
 from pdf_scraper.abstract_doc import AbstractDoc
 from pdf_scraper.hf import HuggingFaceDataset
 from pdf_scraper.pages import AbstractHomePage
-from pdf_scraper.pipeline.PipelineExtendedDataMixin import (
-    PipelineExtendedDataMixin,
-)
+from pdf_scraper.pipeline.PipelineCleanupMixin import PipelineCleanupMixin
+from pdf_scraper.pipeline.PipelineExtendedDataMixin import \
+    PipelineExtendedDataMixin
 from pdf_scraper.pipeline.PipelineMetadataMixin import PipelineMetadataMixin
 from pdf_scraper.readme import ReadMe
 
 log = Log("Pipeline")
 
 
-class Pipeline(PipelineMetadataMixin, PipelineExtendedDataMixin):
+class Pipeline(
+    PipelineMetadataMixin, PipelineExtendedDataMixin, PipelineCleanupMixin
+):
     class DEFAULT:
         MAX_DT = 750.0
 
@@ -33,6 +35,7 @@ class Pipeline(PipelineMetadataMixin, PipelineExtendedDataMixin):
             or Pipeline.DEFAULT.MAX_DT
         )
         log.debug(f"{max_dt=}s")
+        self.cleanup()
         self.scrape_metadata(max_dt)
         self.scrape_extended_data(max_dt)
         ReadMe(self.home_page_class, self.doc_class).build()
