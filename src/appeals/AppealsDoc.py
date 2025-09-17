5dfde40550902e446e781f2963cd1b8e9d3e5352
nuuuwan <nuuuwan@gmail.com>
2025-09-17 13:50:39 +0530

    Use defaults

--- src/appeals/AppealsDoc.py
+++ src/appeals/AppealsDoc.py
@@ -14,36 +14,6 @@ class AppealsDoc(AbstractDoc):
     legistation: str
 
     @classmethod
-    def get_doc_class_label(cls):
-        return "appeals"
-
-    @classmethod
-    def doc_class_pretty_label(cls) -> str:
-        return "⚖️ Judgements of the Court of Appeal of 🇱🇰 Sri Lanka"
-
-    @classmethod
-    def get_remote_data_url_base(cls) -> str:
-        return "/".join(
-            [
-                "https://github.com",
-                "nuuuwan",
-                "lk_judiciary_appeals_court",
-                "tree",
-                "data",  # branch-name
-            ]
-        )
-
-    @cached_property
-    def remote_data_url(self) -> str:
-        return "/".join(
-            [
-                self.get_remote_data_url_base(),
-                self.__class__.get_dir_docs_for_cls_relative(),
-                self.dir_doc_relative_to_class,
-            ]
-        )
-
-    @classmethod
     def gen_docs(cls) -> Generator["AppealsDoc", None, None]:
         for data_page in AppealsHomePage().gen_data_pages():
             for d in data_page.gen_dicts():
