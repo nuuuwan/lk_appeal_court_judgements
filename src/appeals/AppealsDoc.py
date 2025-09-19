59044ac01a8e41a3eb5383bd8eda0798f69e7549
nuuuwan <nuuuwan@gmail.com>
2025-09-19 13:02:01 +0530

    Added get_doc_class_description and get_doc_class_emoji

--- src/appeals/AppealsDoc.py
+++ src/appeals/AppealsDoc.py
@@ -13,6 +13,19 @@ class AppealsDoc(AbstractPDFDoc):
     legistation: str
 
     @classmethod
+    def get_doc_class_description(cls) -> str:
+        return "\n\n".join(
+            [
+                "An appeals court judgment (also called an appellate court judgment) is a ruling delivered by a higher court that reviews decisions made by lower courts. It ensures that justice is applied consistently, correcting legal or procedural errors that may have occurred at trial.",  # noqa: E501
+                "In Sri Lanka, the Court of Appeal plays this role, shaping how laws are interpreted and setting important legal precedents that influence future cases. These judgments are central to upholding fairness, protecting rights, and strengthening the rule of law.",  # noqa: E501
+            ]
+        )
+
+    @classmethod
+    def get_doc_class_emoji(cls) -> str:
+        return "⚖️"
+
+    @classmethod
     def gen_docs(cls) -> Generator["AppealsDoc", None, None]:
         for data_page in AppealsHomePage().gen_data_pages():
             for d in data_page.gen_dicts():
